f34d9779fb5b617088f20684478312c682899155
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-26 19:10:15 +0700

    void search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -76,3 +76,20 @@ public:
             }
         }
     }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        // This function searches the currenNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+    
+}
