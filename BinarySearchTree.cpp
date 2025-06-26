f937942160f223b393a522391962aa283a62b716
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-26 18:23:27 +0700

    membuat fungsi insert

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -25,7 +25,22 @@ public:
         ROOT = nullptr; //Initializing ROOT to null 
     }
 
-    
+    void insert()
+    {
+        int x;
+        cout << "Masukan nilai: ";
+        cin >> x;
+
+        // step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+        //Step 2: Assign value to the data field of new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+    }
 }
 
 int main(){
