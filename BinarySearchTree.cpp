9f1c3a7bf103e48045b17db3dcec2aaab2b15123
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-26 19:11:09 +0700

    void inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -96,4 +96,14 @@ public:
         // Check if the tree is empty
         return ROOT = nullptr;
     }
-  
+    void inorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+                return;
+
+}
